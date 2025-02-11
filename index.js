87ffe9b123ea62eb4381635fd91941e6992aec66
Arturo Benjamin Vazquez Munoz <marturo@tresguerras.com.mx>
2025-02-10 20:52:27 -0600

    configuracion del puerto y la carpeta publica

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("hola mundoXXXXXXS");
+const express = require('express');
+require('dotenv').config();
+
+console.log(process.env);
+
+
+// Crear  el servidor de express
+
+const app  = express();
+
+// rutas 
+
+
+// Directorio publico 
+app.use( express.static('public') )
+
+// app.get('/', (  req , res ) =>{
+
+//   // console.log("Se requiere el /");
+//   res.json({
+//     ok : true
+//   })
+
+// } ) ;
+
+// escuchar peticiones
+
+const puerto = 3001
+
+app.listen(process.env.PORT , () => {
+
+  console.log(`Servidor en puerto ${process.env.PORT} `);
+  
+
+});
