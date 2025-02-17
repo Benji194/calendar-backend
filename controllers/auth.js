0b772105b387574742ba755d54403e3281166999
Arturo Benjamin Vazquez Munoz <marturo@tresguerras.com.mx>
2025-02-17 16:58:54 -0600

    crear un usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,39 @@
 const  { response } = require('express');
+
+const  bcrypt  = require('bcryptjs')
+
 const Usuario = require('../models/Usuario')
 
 const crearUsuario = async (  req , res = response ) =>{
 
-  // const {  name , email  , password } = req.body;
+  const {   email  , password } = req.body;
 
   try {
 
-    const usuario = new Usuario( req.body );
+    let usuario = await Usuario.findOne({ email  });
+    
+    if ( usuario ) {
+      
+      return res.status(400).json({
+        ok : false,
+        mgs : 'Eso correo ya fue utilizado intenta con otro'
+      })
+    }
+    
+
+    usuario = new Usuario( req.body );
+
+    // Encriptar contraseña 
+    const salt =  bcrypt.genSaltSync(); 
+    usuario.password = bcrypt.hashSync( password , salt );
+
 
     await usuario.save();
   
     res.status(201).json({
       ok : true,
-      msg: 'registro',
+      uid: usuario._id,
+      name: usuario.name
     });
     
   } catch (error) {
