41fe15050fc70d53c02069458e26cbe0c28e6a65
Arturo Benjamin Vazquez Munoz <marturo@tresguerras.com.mx>
2025-02-19 17:19:17 -0600

    mejora para leer el validarJWT

--- routes/events.js
+++ routes/events.js
@@ -7,16 +7,18 @@ const { validarJWT } = require('../middlewares/validar-jwt');
 const router = Router();
 
 // Todas pasan por la validacion del jwt 
+router.use(validarJWT);
+
 //  obtener eventos  
-router.get('/', validarJWT , getEventos   );
+router.get('/' , getEventos   );
 
 // Crear un nuevo evento
-router.post('/', validarJWT , crearEvento   );
+router.post('/' , crearEvento   );
 
 // Actualizar evento
-router.put('/:id', validarJWT , actualizarEvento );
+router.put('/:id' , actualizarEvento );
 
 // Borrar evento 
-router.delete('/:id', validarJWT , eliminarEvento );
+router.delete('/:id' , eliminarEvento );
 
 module.exports = router;
